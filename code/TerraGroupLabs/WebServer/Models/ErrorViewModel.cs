8a38850abacd5b9b68c9bea0262872ae3a3d355e
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-03-25 22:30:14 +0100

    Init commit2

--- code/TerraGroupLabs/WebServer/Models/ErrorViewModel.cs
+++ code/TerraGroupLabs/WebServer/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace WebServer.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace WebServer.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
