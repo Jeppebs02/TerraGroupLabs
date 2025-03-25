0a44ffa2e5343a9af7576d8c5963979eda8fb545
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-03-26 00:30:08 +0100

    configs

--- code/TerraGroupLabs/WebAPI/Program.cs
+++ code/TerraGroupLabs/WebAPI/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+// Add configuration from appsettings.json and environment variables
+builder.Configuration.AddJsonFile("/Configs/appsettings.json", optional: false, reloadOnChange: true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
