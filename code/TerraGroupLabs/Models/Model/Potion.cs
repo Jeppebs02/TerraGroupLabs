3df3ef47220be37c8fb235f44b9253370841bfaa
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-03-25 23:39:05 +0100

    minor changes

---
+++ code/TerraGroupLabs/Models/Model/Potion.cs
@@ -0,0 +1,19 @@
+﻿namespace Models;
+
+public class Potion
+{
+    public int id {get;set;}
+    public string name {get;set;}
+    public double price {get;set;}
+
+    public Potion(int id_,string name_,double price_)
+    {
+        this.id = id_;
+        this.name = name_;
+        this.price = price_;
+    }
+    
+    
+    
+    
+}
