1067a629becfae5eed2d753e653e0b36e6bc853e
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-03-26 12:52:53 +0100

    view

--- code/TerraGroupLabs/WebServer/Controllers/PotionController.cs
+++ code/TerraGroupLabs/WebServer/Controllers/PotionController.cs
@@ -1,18 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
+using Models.APIRequester;
+using Models.Model;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace WebServer.Controllers
 {
     public class PotionController : Controller
     {
-        
         //Potion API requests will be handled here
-        
-        
-        
-        // GET: PotionController
-        public ActionResult Index()
+        private readonly ApiRequester _apiRequester;
+
+        public PotionController(ApiRequester apiRequester)
         {
-            return View();
+            _apiRequester = apiRequester;
+        }
+
+        public PotionController(IConfiguration configuration = null)
+        {
+            if (configuration == null)
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("Webserver/Configs/appsettings.json", optional: false, reloadOnChange: true);
+                configuration = builder.Build();
+            }
+            _apiRequester = new ApiRequester(configuration);
+        }
+
+        // GET: PotionController
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                string jsonResponse = await _apiRequester.GetAsync("potion/AllPotions");
+                List<Potion> potions = JsonConvert.DeserializeObject<List<Potion>>(jsonResponse);
+                return View("Index", potions); // Ensure the view name is correctly specified
+            }
+            catch (Exception ex)
+            {
+                // Handle exception (log it, show error message, etc.)
+                return View("Index", new List<Potion>()); // Ensure the view name is correctly specified
+            }
         }
 
         // GET: PotionController/Details/5
