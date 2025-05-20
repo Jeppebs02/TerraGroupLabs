acfa0bcb61dadc49fc2c188307d560d6deec6e8a
Jeppebs02 <32048740+Jeppebs02@users.noreply.github.com>
2025-05-20 21:49:50 +0200

    it works

--- code/TerraGroupLabs/server/Lib.cs
+++ code/TerraGroupLabs/server/Lib.cs
@@ -2,30 +2,138 @@ using SpacetimeDB;
 
 public static partial class Module
 {
-    [SpacetimeDB.Table]
-    public partial struct Person
+
+    #region Tables
+
+    [Table(Name = "user", Public = true)]
+    public partial class User
     {
-        [SpacetimeDB.AutoInc]
-        [SpacetimeDB.PrimaryKey]
-        public int Id;
-        public string Name;
-        public int Age;
+        [PrimaryKey]
+        public Identity Identity;
+        public string? Name;
+        public bool Online;
     }
 
-    [SpacetimeDB.Reducer]
-    public static void Add(ReducerContext ctx, string name, int age)
+    [Table(Name = "message", Public = true)]
+    public partial class Message
     {
-        var person = ctx.Db.Person.Insert(new Person { Name = name, Age = age });
-        Log.Info($"Inserted {person.Name} under #{person.Id}");
+        public Identity Sender;
+        public Timestamp Sent;
+        public string Text = "";
     }
 
-    [SpacetimeDB.Reducer]
-    public static void SayHello(ReducerContext ctx)
+
+    #endregion
+
+    #region Reducers
+
+    [Reducer]
+    public static void SetName(ReducerContext ctx, string name)
     {
-        foreach (var person in ctx.Db.Person.Iter())
+        name = ValidateName(name);
+
+        var user = ctx.Db.user.Identity.Find(ctx.Sender);
+        if (user is not null)
         {
-            Log.Info($"Hello, {person.Name}!");
+            user.Name = name;
+            ctx.Db.user.Identity.Update(user);
         }
-        Log.Info("Hello, World!");
     }
+
+
+    [Reducer]
+    public static void SendMessage(ReducerContext ctx, string text)
+    {
+        text = ValidateMessage(text);
+        Log.Info(text);
+        ctx.Db.message.Insert(
+            new Message
+            {
+                Sender = ctx.Sender,
+                Text = text,
+                Sent = ctx.Timestamp,
+            }
+        );
+    }
+
+    [Reducer(ReducerKind.ClientConnected)]
+    public static void ClientConnected(ReducerContext ctx)
+    {
+        Log.Info($"Connect {ctx.Sender}");
+        var user = ctx.Db.user.Identity.Find(ctx.Sender);
+
+        if (user is not null)
+        {
+            // If this is a returning user, i.e., we already have a `User` with this `Identity`,
+            // set `Online: true`, but leave `Name` and `Identity` unchanged.
+            user.Online = true;
+            ctx.Db.user.Identity.Update(user);
+        }
+        else
+        {
+            // If this is a new user, create a `User` object for the `Identity`,
+            // which is online, but hasn't set a name.
+            ctx.Db.user.Insert(
+                new User
+                {
+                    Name = null,
+                    Identity = ctx.Sender,
+                    Online = true,
+                }
+            );
+        }
+    }
+
+    [Reducer(ReducerKind.ClientDisconnected)]
+    public static void ClientDisconnected(ReducerContext ctx)
+    {
+        var user = ctx.Db.user.Identity.Find(ctx.Sender);
+
+        if (user is not null)
+        {
+            // This user should exist, so set `Online: false`.
+            user.Online = false;
+            ctx.Db.user.Identity.Update(user);
+        }
+        else
+        {
+            // User does not exist, log warning
+            Log.Warn("Warning: No user found for disconnected client.");
+        }
+    }
+
+
+
+
+    #endregion
+
+    #region Methods
+
+    /// Takes a name and checks if it's acceptable as a user's name.
+    private static string ValidateName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new Exception("Names must not be empty");
+        }
+        return name;
+    }
+
+
+    /// Takes a message's text and checks if it's acceptable to send.
+    private static string ValidateMessage(string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            throw new ArgumentException("Messages must not be empty");
+        }
+        return text;
+    }
+
+    #endregion
+
+
+
+
+
 }
